745e7cf3edfefc7024de203f71f53a1dc44d1cec
MusangQim <musangqim03@gmail.com>
2026-01-23 18:48:59 +0800

    update file

--- stack_utils.c
+++ stack_utils.c
@@ -6,30 +6,16 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 12:17:22 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/23 18:31:43 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/23 18:48:45 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted(t_node *stack)
-{
-	t_node	*current;
-
-	current = stack;
-	while (current && current->next)
-	{
-		if (current->value > current->next->value)
-			return (0);
-		current = current->next;
-	}
-	return (1);
-}
-
 int	min(t_node *stack)
 {
 	t_node		*current;
-	int	min_value;
+	int			min_value;
 
 	if (!stack)
 		return (0);
@@ -44,4 +30,36 @@ int	min(t_node *stack)
 	return (min_value);
 }
 
+int	max(t_node *stack)
+{
+	t_node		*current;
+	int			max_value;
 
+	if (!stack)
+		return (0);
+	max_value = stack->value;
+	current = stack;
+	while (current)
+	{
+		if (current->value > max_value)
+			max_value = current->value;
+		current = current->next;
+	}
+	return (max_value);
+}
+
+int	is_sorted(t_node *stack)
+{
+	t_node	*current;
+
+	if (!stack || !stack->next)
+		return (1);
+	current = stack;
+	while (current->next)
+	{
+		if (current->value > current->next->value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
