bd31080176313d55c19cc1ec1b456760997917a8
MusangQim <musangqim03@gmail.com>
2026-01-24 20:01:56 +0800

    add parse file

---
+++ parse.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/24 18:16:29 by adzmusta          #+#    #+#             */
+/*   Updated: 2026/01/24 19:28:50 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	parse_args(argc, **argv, &stack_a)
+{
+	char	**numbers;
+	int		i;
+
+	if (argc == 2)
+		numbers = ft_split(argv[1], ' ');
+	else
+		numbers = &argv[1];
+	i = 0;
+	while (numbers[i])
+	{
+
