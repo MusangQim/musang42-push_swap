bcc7fac4585d6d712051b4217dc44a510a947b95
MusangQim <musangqim03@gmail.com>
2026-01-29 14:23:48 +0800

    update files

--- swap.c
+++ swap.c
@@ -6,25 +6,34 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 13:49:15 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/29 13:50:16 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/29 14:21:12 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap_process
+void	swap_process(t_node **stack)
 {
+	t_node	tmp;
+
+	if (**stack < 2)
+		return ;
+	tmp = *stack
+
 }
 
-void	swap_a
+void	swap_a(t_node **stack_a)
 {
+
 }
 
-void	swap_b
+void	swap_b(t_node **stack_b)
 {
+
 }
 
-void	swap_s
+void	swap_s(t_node **stack_a, t_node **stack_b)
 {
+
 }
 
