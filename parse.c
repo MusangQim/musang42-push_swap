d09e509c116672364d554b3436eb15eb4042e84d
MusangQim <musangqim03@gmail.com>
2026-01-30 18:52:05 +0800

    update file

--- parse.c
+++ parse.c
@@ -6,11 +6,21 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 18:16:29 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/30 18:28:31 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/30 18:51:47 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		free(split[i++]);
+	free(split);
+}
 
 void	parse_args(int argc, char **argv, t_stack **stack_a)
 {
@@ -19,6 +29,13 @@ void	parse_args(int argc, char **argv, t_stack **stack_a)
 
 	if (argc == 2)
 		numbers = ft_split(argv[1], ' ');
+	else
+		numbers = &argv[1];
 	i = 0;
 	while (numbers[i])
+	{
+		i++;
+	}
+	if (argc == 2)
+		free_split(numbers);
 }
