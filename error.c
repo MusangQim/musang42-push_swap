624e2f14f7a0e900812cc111fe26748bef3db961
MusangQim <musangqim03@gmail.com>
2026-01-31 21:26:56 +0800

    check invalidchar added

--- error.c
+++ error.c
@@ -6,14 +6,28 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 20:17:37 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/31 20:55:57 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/31 21:26:06 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	check_invalidchar()
+static void	check_invalidchar(char *str)
 {
+	int	j;
+
+	j = 0;
+	if (str[j] == '+' || str[j] == '-')
+		j++;
+	while (str[j] != '\0')
+	{
+		if (!ft_isdigit(str[j]))
+		{
+			ft_putstr("Error\n");
+			exit(1);
+		}
+		j++;
+	}
 }
 
 static void	check_sign()
@@ -28,11 +42,17 @@ static void	check_duplicate()
 {
 }
 
-void	check_errors(argv)
+void	check_errors(char **argv)
 {
 	int	i;
-	int	j;
 
-	i = 0;
-	j = 0;
+	i = 1;
+	while (argv[i])
+	{
+		check_invalidchar(argv[i]);
+		check_sign(argv[i]);
+		check_overflow(argv[i]);
+		i++;
+	}
+	check_duplicate(argv);
 }
