5e3e4e4c7e6111a38dbbae96aa0d5e971737afa6
MusangQim <musangqim03@gmail.com>
2026-02-07 21:16:38 +0800

    try and error to understand index.c

--- index.c
+++ index.c
@@ -6,10 +6,17 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 13:11:23 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/05 13:15:27 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/07 21:16:18 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static	void	find_min_unindex(t_node *stack)
+{
+}
+
+void	assign_index(t_node *stack)
+{
+}
 
