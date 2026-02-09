43002106733a46f4675d64b039942d7721697b5d
MusangQim <musangqim03@gmail.com>
2026-02-09 20:36:43 +0800

    update sort_2 f(x)

--- sort_small.c
+++ sort_small.c
@@ -6,14 +6,21 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 20:21:28 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/09 20:32:24 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/09 20:36:24 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	sort_2()
+int	sort_2(t_node **stack_a)
 {
+	t_node	*node;
+	int	sort;
+
+	sort = is_sorted(stack_a);
+	if (sort)
+		return (0);
+	if 
 
 }
 
