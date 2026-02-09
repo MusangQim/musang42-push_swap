637ae06ee203c6f6485bfd71aa45c5ac36855f6c
MusangQim <musangqim03@gmail.com>
2026-02-09 20:32:29 +0800

    add sorting small function

--- sort_small.c
+++ sort_small.c
@@ -6,22 +6,29 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 20:21:28 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/09 20:22:19 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/09 20:32:24 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	sort_small(t_node **stack_a)
-{
-}
-
 int	sort_2()
 {
+
 }
 
 int	sort_3()
 {
+
 }
 
-int	
+int	sort_5(t_node)
+{
+
+}
+
+int	sort_small(t_node **stack_a)
+{
+
+}
+
