1c9e16781e46ca68a71ab41811f3b49ca4eefd2a
MusangQim <musangqim03@gmail.com>
2026-02-09 23:28:16 +0800

    update sort_2 function

--- sort_small.c
+++ sort_small.c
@@ -6,25 +6,25 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 20:21:28 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/09 23:25:50 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/09 23:27:44 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	sort_2(t_node **stack_a)
+void	sort_2(t_node **stack_a)
 {
 	int	sort;
 
 	sort = is_sorted(stack_a);
 	if (sort)
 		return (0);
-	if ((*stack_a)->value) > ((*stack_a)->next->value)
+	if ((*stack_a)->value > (*stack_a)->next->value)
 		swap_a(stack_a);
 	return (stack_a);
 }
 
-int	sort_3()
+void	sort_3()
 {
 
 }
