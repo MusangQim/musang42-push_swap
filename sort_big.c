7194049c94fa565ee443528db44658ad189fc797
MusangQim <musangqim03@gmail.com>
2026-02-15 15:13:53 +0800

    update file

--- sort_big.c
+++ sort_big.c
@@ -6,32 +6,16 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 20:32:12 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/14 20:00:41 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/15 15:13:44 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_chunk(t_node **stack_a, t_node **stack_b, int size)
-{
-	int	element;
-}
-
-void	push_back(t_node **stack_a, t_node **stack_b)
-{
-}
-
-void	max_position()
-{
-}
-
-void	move_top()
-{
-}
-
 void	sort_big(t_node **stack_a, t_node **stack_b)
 {
 	int	size;
+	int	chunk_size;
 
 	if (is_sorted(*stack_a))
 		return ;
