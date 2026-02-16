f6b5f0f5f7bce359c1a33384223115ac4a93de69
MusangQim <musangqim03@gmail.com>
2026-02-16 16:10:09 +0800

    update header file

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,18 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 21:24:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/07 20:26:57 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/16 16:09:48 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 typedef struct s_node
 {
-	int			value;
-	int			index;
-	struct s_node		*next;
+	int				value;
+	int				index;
+	int				moves_a;
+	int				moves_b;
+	int				direction_a;
+	int				direction_b;
+	int				total_cost;
+	struct s_node	*next;
 }	t_node;
