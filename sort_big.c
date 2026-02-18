f3cd5700829fe32e11ca61dd5606809b77721aef
MusangQim <musangqim03@gmail.com>
2026-02-18 12:58:23 +0800

    delete unused f(x) and update file for rebuild stack

--- sort_big.c
+++ sort_big.c
@@ -6,18 +6,25 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 20:32:12 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/16 17:32:11 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/18 12:57:35 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_chunks()
+void	rebuild_stack(t_node **stack_a, t_node **stack_b)
 {
-}
+	int	max_index;
 
-void	push_to_a()
-{
+	while (stack_b != NULL)
+	{
+		max_index = max(stack_b);
+		if (max_index <= stack_size(stack_b) / 2)
+			rotate_b(stack_b);
+		else
+			revrotate(stack_b);
+	}
+	push_a(stack_a);
 }
 
 int	find_target_position(t_node **stack_a, t_node **target_node, int element_index)
@@ -44,41 +51,6 @@ int	find_target_position(t_node **stack_a, t_node **target_node, int element_ind
 	}
 	return (position);
 }
-
-void	calculate_cost(t_node **stack_a, t_node *node_b, t_node *stack_b)
-{
-	int			pos_in_a;
-	int			pos_in_b;
-	t_node		*target_node;
-
-	pos_in_a = find_target_position(stack_a, &target_node, node_b->index);
-	pos_in_b = position(stack_b, stack_b->node);
-	if (pos_in_a <= stack_size(stack_a) / 2)
-	{
-		direction_a = rotate_a(stack_a);
-		moves_a = pos_in_a;
-	}
-	else
-	{
-		direction_a = revrotate_a(stack_a);
-		moves_a = stack_size(stack_a) - pos_in_a;
-	}
-	if (pos_in_b <= stack_size(stack_b) / 2)
-	{
-		direction_b = rotate_b(stack_b);
-		moves_b = pos_in_b;
-	}
-	else
-	{
-		direction_b = revrotate_b(stack_b);
-		moves_b = stack_size(stack_b) - pos_in_b;
-	}
-	if (direction_a == direction_b)
-		total_cost = max(moves_a, moves_b);
-	else
-		total_cost = moves_a + moves_b;			
-}
-
 
 void	sort_big(t_node **stack_a, t_node **stack_b)
 {
