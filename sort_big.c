8c9bccd08497b165a2a7fe0f4d0e22b3db48b6ca
MusangQim <musangqim03@gmail.com>
2026-02-20 11:15:15 +0800

    update

--- sort_big.c
+++ sort_big.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 20:32:12 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/20 11:10:14 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/20 11:15:08 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	push_chunks(t_node **stack_a, t_node **stack_b, int chunksize)
 		}
 		else
 			rotate_a(stack_a);
-		if (push_in == currentlimit)
+		if (push_in == chunksize)
 		{
 			currentlimit += chunksize;
 			push_in = 0;
