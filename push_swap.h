5509f53569298ff8d693f32b33b1b4dd11456289
MusangQim <musangqim03@gmail.com>
2026-02-21 10:20:13 +0800

    update header file

--- push_swap.h
+++ push_swap.h
@@ -6,9 +6,15 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 21:24:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/18 12:41:29 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/21 10:19:51 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_node
 {
@@ -16,3 +22,16 @@ typedef struct s_node
 	int				index;
 	struct s_node	*next;
 }	t_node;
+
+/* STACK */
+t_node	*create_node(int value);
+void	stack_push(t_node **stack, t_node *new);
+int		stack_size(t_node *stack);
+
+/* STACK UTILS */
+int		min(t_node *stack);
+int		max(t_node *stack);
+int		is_sorted(t_node *stack);
+int		position(t_node *stack, int value);
+
+#endif
