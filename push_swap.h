84667bbf0556d9e2311664eb6c43076afd583551
MusangQim <musangqim03@gmail.com>
2026-02-21 14:42:49 +0800

    modify and add all f(x) in header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 21:24:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/21 10:19:51 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/21 14:42:26 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,48 @@ int		min(t_node *stack);
 int		max(t_node *stack);
 int		is_sorted(t_node *stack);
 int		position(t_node *stack, int value);
+
+/* PARSE */
+void	parse_args(int argc, char **argv, t_node **stack_a);
+
+/* ERROR */
+void	check_errors(char **argv, t_node **stack_a);
+
+/* ERROR UTILS */
+int		help_overflow(long num, int digit, int sign);
+
+/* INDEX */
+void	assign_index(t_node *stack);
+
+/* OPERATION */
+void	push_a(t_node **stack_a, t_node **stack_b);
+void	push_b(t_node **stack_a, t_node **stack_b);
+
+void	swap_a(t_node **stack_a);
+void	swap_b(t_node **stack_b);
+void	swap_s(t_node **stack_a, t_node **stack_b);
+
+void	rotate_a(t_node **stack_a);
+void	rotate_b(t_node **stack_b);
+void	rotate_r(t_node **stack_a, t_node **stack_b);
+
+void	revrotate_a(t_node **stack_a);
+void	revrotate_b(t_node **stack_b);
+void	revrotate_r(t_node **stack_a, t_node **stack_b);
+
+/* SORT SMALL */
+void	sort_2(t_node **stack_a);
+void	sort_3(t_node **stack_a);
+void	sort_4(t_node **stack_a);
+void	sort_5(t_node **stack_a);
+void	sort_small(t_node **stack_a);
+
+/* SORT BIG */
+void	push_chunks(t_node **stack_a, t_node **stack_b);
+int		find_max_index(t_node *stack);
+void	rebuild_stack(t_node **stack_a, t_node **stack_b);
+
+/* UTILS */
+int	ft_putstr(char *s);
 
 #endif
