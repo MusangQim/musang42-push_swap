5ae51bebdac86612753d7ad25daf3987f045da9b
MusangQim <musangqim03@gmail.com>
2026-02-22 23:53:25 +0800

    add free stack function

--- utils.c
+++ utils.c
@@ -6,12 +6,20 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 14:39:43 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/21 15:19:32 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/22 23:53:15 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_putstr(char *s)
+void	free_stack(t_node **stack)
 {
+	t_node	*temp;
+
+	while (*stack)
+	{
+		temp = (*stack)->next;
+		free(*stack);
+		*stack = temp;
+	}
 }
