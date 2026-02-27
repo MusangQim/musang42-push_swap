9f59211798cfbe815abb4fcc5f7aa1de88ddbe3b
MusangQim <musangqim03@gmail.com>
2026-02-27 23:20:13 +0800

    try rearrange the line code

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 18:16:29 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/24 16:11:00 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/02/27 23:19:58 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,14 @@ void	parse_args(int argc, char **argv, t_node **stack_a)
 	char	**numbers;
 
 	if (argc == 2)
+	{
 		numbers = ft_split(argv[1], ' ');
-	else
-		numbers = &argv[1];
-	check_errors(numbers, stack_a);
-	if (argc == 2)
+		check_errors(numbers, stack_a, 0);
 		free_split(numbers);
+	}
+	else
+	{
+		numbers = &argv[1];
+		check_errors(numbers, stack_a, 0);
+	}
 }
