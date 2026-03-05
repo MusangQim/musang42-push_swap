7bebcd77bed69801971129702055f8684c70cf9c
MusangQim <musangqim03@gmail.com>
2026-03-05 09:26:59 +0800

    let 3 function move to sortbig_utils

--- sort_big.c
+++ sort_big.c
@@ -6,61 +6,11 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 20:32:12 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/03/05 04:20:10 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/03/05 09:25:53 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	cost_a(t_node *stack_a, int index)
-{
-	int	pos;
-	int	size;
-
-	pos = position_by_index(stack_a, index);
-	size = stack_size(stack_a);
-	if (pos <= size / 2)
-		return (pos);
-	else
-		return (size - pos);
-}
-
-int	cost_b(t_node *stack_b, int index)
-{
-	int	pos;
-	int	size;
-
-	pos = position_by_index(stack_b, index);
-	size = stack_size(stack_b);
-	if (pos <= size / 2)
-		return (pos);
-	else
-		return (size - pos);
-}
-
-int	find_cheapest(t_node *stack_b, t_node *stack_a)
-{
-	t_node		*current;
-	int			min_cost;
-	int			total_cost;
-	int			cheapest;
-
-	current = stack_b;
-	min_cost = INT_MAX;
-	cheapest = 0;
-	while (current != NULL)
-	{
-		total_cost = cost_b(stack_b, current->index)
-			+ cost_a(stack_a, current->index);
-		if (total_cost < min_cost)
-		{
-			min_cost = total_cost;
-			cheapest = current->index;
-		}
-		current = current->next;
-	}
-	return (cheapest);
-}
 
 /* push all stack A to stack B */
 void	push_chunks(t_node **stack_a, t_node **stack_b)
@@ -111,13 +61,15 @@ void	rebuild_stack(t_node **stack_a, t_node **stack_b)
 
 	while (*stack_b != NULL)
 	{
+		cheapest = find_cheapest(*stack_b, *stack_a);
 		pos = position_by_index(*stack_b, cheapest);
 		size = stack_size(*stack_b);
-		cheapest = find_cheapest(*stack_b, *stack_a);
 		if (pos <= size / 2)
-			rotate_b(stack_b);
+			while ((*stack_b)->index != cheapest)
+				rotate_b(stack_b);
 		else
-			revrotate_b(stack_b);
+			while ((*stack_b)->index != cheapest)
+				revrotate_b(stack_b);
 		push_a(stack_a, stack_b);
 	}
 }
