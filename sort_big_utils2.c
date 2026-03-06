5feae9e1252e7a26671dfc957e8a6f661c63dc26
MusangQim <musangqim03@gmail.com>
2026-03-06 14:24:43 +0800

    add 3 function help the rebuild stack function

--- sort_big_utils2.c
+++ sort_big_utils2.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 14:15:12 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/03/06 14:17:36 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/03/06 14:23:32 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,37 @@ void	shift_stacks(t_node **stack_a)
 			revrotate_a(stack_a);
 }
 
-void
+void	rotate_top_b(t_node **stack_b, int cheapest)
+{
+	int	pos;
+	int	size;
+
+	pos = position_by_index(*stack_b, cheapest);
+	size = stack_size(*stack_b);
+	if (pos <= size / 2)
+		while ((*stack_b)->index != cheapest)
+			rotate_b(stack_b);
+	else
+		while ((*stack_b)->index != cheapest)
+			revrotate_b(stack_b);
+}
+
+void	rotate_pos_a(t_node **stack_a, int target_pos)
+{
+	int	count;
+	int	size;
+
+	size = stack_size(*stack_a);
+	if (target_pos <= size / 2)
+	{
+		count = target_pos;
+		while (count-- > 0)
+			rotate_a(stack_a);
+	}
+	else
+	{
+		count = size - target_pos;
+		while (count-- > 0)
+			revrotate_a(stack_a);
+	}
+}
