be3fe201807f004f280897a53c3a6e446ea8e8fe
MusangQim <musangqim03@gmail.com>
2026-03-07 18:52:16 +0800

    int to long convert it

--- error_utils.c
+++ error_utils.c
@@ -6,32 +6,31 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 18:55:03 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/02/22 23:58:54 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/03/07 18:52:06 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	help_overflow(long num, int digit, int sign)
+long	help_overflow(long num, int digit, int sign)
 {
-	if (sign == +1)
+	if (sign == 1)
 	{
 		if (num > (INT_MAX - digit) / 10)
 		{
-			ft_putstr("Error\n");
+			write (2, "Error\n", 6);
 			exit(1);
 		}
 	}
-	if (sign == -1)
+	else
 	{
 		if (num > ((long)INT_MAX + 1 - digit) / 10)
 		{
-			ft_putstr("Error\n");
+			write (2, "Error\n", 6);
 			exit(1);
 		}
 	}
-	num = num * 10 + digit;
-	return (num);
+	return (num * 10 + digit);
 }
 
 void	error_exit(t_node **stack)
